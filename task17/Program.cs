5aefaf6c68d38ce94ff0b1e14ba028b668fe7f84
yourenoticed <yourenoticed@gmail.com>
2023-06-26 15:01:22 +0700

    did task17 using methods for each case

--- task17/Program.cs
+++ task17/Program.cs
@@ -1,2 +1,79 @@
-﻿Console.Write("Enter a number: ");
+﻿void A4InTwo()
+{
+    Console.Write("Enter a number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    number *= number;
+    Console.WriteLine(number);
+    number *= number;
+    Console.WriteLine(number);
+}
 
+void A6InThree()
+{
+    Console.Write("Enter a number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int powerof = number;
+    number *= number;
+    Console.WriteLine(number);
+    number *= powerof;
+    Console.WriteLine(number);
+    number *= number;
+    Console.WriteLine(number);
+}
+
+void A7InFour()
+{
+    Console.Write("Enter a number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int powerof = number;
+    number *= number;
+    Console.WriteLine(number);
+    number *= powerof;
+    Console.WriteLine(number);
+    number *= number;
+    Console.WriteLine(number);
+    number *= powerof;
+    Console.WriteLine(number);
+}
+
+void A8InThree()
+{
+    Console.Write("Enter a number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    number *= number;
+    Console.WriteLine(number);
+    number *= number;
+    Console.WriteLine(number);
+    number *= number;
+    Console.WriteLine(number);
+}
+
+void A9InFour()
+{
+    Console.Write("Enter a number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int powerof = number;
+    number *= number;
+    Console.WriteLine(number);
+    number *= number;
+    Console.WriteLine(number);
+    number *= number;
+    Console.WriteLine(number);
+    number *= powerof;
+    Console.WriteLine(number);
+}
+
+void A10InFour()
+{
+    Console.Write("Enter a number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int powerof = number;
+    number *= number;
+    Console.WriteLine(number);
+    number *= number;
+    Console.WriteLine(number);
+    number *= powerof;
+    Console.WriteLine(number);
+    number *= number;
+    Console.WriteLine(number);
+}
